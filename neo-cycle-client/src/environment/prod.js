3e07a7770f91264cd206cfb13ac8ede40916ef62
長根尾貴仁 <taakaaneo@gmail.com>
2020-04-06 21:38:53 +0900

    feat: create env file in client

---
+++ neo-cycle-client/src/environment/prod.js
@@ -0,0 +1,10 @@
+const config = {
+  Region: 'ap-northeast-1',
+  UserPoolId: 'ap-northeast-1_<UNKNOWN>',
+  ClientId: '<UNKNOWN>',
+  IdentityPoolId: 'ap-northeast-1:<UNKNOWN>',
+  InvokeUrl: 'https://<UNKNOWN>.execute-api.ap-northeast-1.amazonaws.com/dev',
+  policyArn: ''
+}
+
+module.exports = config 
