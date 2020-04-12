c9f08291aa5ec1b6b899f343e396686c896057c3
長根尾貴仁 <taakaaneo@gmail.com>
2020-04-12 13:41:54 +0900

    feat: modify ApiInvoker  not to make body JSON.parse()

--- neo-cycle-client/src/api/cycle-management.js
+++ neo-cycle-client/src/api/cycle-management.js
@@ -4,20 +4,19 @@ const url = 'https://zn2vxjwu4d.execute-api.ap-northeast-1.amazonaws.com/dev';
 
 export async function retrieveParkingList() {
   const res = await axios.post(url + '/parkings');
-  return JSON.parse(res.data.body)
+  return res.data
 }
-
 export async function checkStatus() {
   const res = await axios.post(url + '/status');
-  return JSON.parse(res.data.body)
+  return res.data
 }
 
 export async function makeReservation(cycle) {
   const res = await axios.post(url + '/reservation', cycle);
-  return JSON.parse(res.data.body)
+  return res.data
 }
 
 export async function cancelReservation() {
   const res = await axios.post(url + '/cancellation');
-  return JSON.parse(res.data.body)
+  return res.data
 }
