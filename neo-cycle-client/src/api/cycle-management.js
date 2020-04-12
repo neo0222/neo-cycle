baf38aab967df9eacfc3f009e22948b5d478e5ee
長根尾貴仁 <taakaaneo@gmail.com>
2020-04-12 11:42:11 +0900

    feat: modify api url and env info about cognito

--- neo-cycle-client/src/api/cycle-management.js
+++ neo-cycle-client/src/api/cycle-management.js
@@ -1,6 +1,6 @@
 const axios = require('axios');
 import env from '../environment/index'
-const url = 'https://bmamuk9hjf.execute-api.ap-northeast-1.amazonaws.com/prod';
+const url = 'https://zn2vxjwu4d.execute-api.ap-northeast-1.amazonaws.com/dev';
 
 export async function retrieveParkingList() {
   const res = await axios.post(url + '/parkings');
--- neo-cycle-client/src/environment/dev.js
+++ neo-cycle-client/src/environment/dev.js
@@ -1,9 +1,9 @@
 const config = {
   Region: 'ap-northeast-1',
-  UserPoolId: 'ap-northeast-1_<UNKNOWN>',
-  ClientId: '<UNKNOWN>',
-  IdentityPoolId: 'ap-northeast-1:<UNKNOWN>',
-  InvokeUrl: 'https://<UNKNOWN>.execute-api.ap-northeast-1.amazonaws.com/dev',
+  UserPoolId: 'ap-northeast-1_QQRHYdhFC',
+  ClientId: '1bjjhjcspk531cm0saf723gu6g',
+  IdentityPoolId: 'ap-northeast-1:2d02fd40-8cf3-4418-8e36-607e5a789642',
+  InvokeUrl: 'https://zn2vxjwu4d.execute-api.ap-northeast-1.amazonaws.com/dev',
   policyArn: ''
 }
 
