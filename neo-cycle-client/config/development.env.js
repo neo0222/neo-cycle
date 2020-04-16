af0bc3f17d0cddf9df470d57bcdc22b5fbf4b376
長根尾貴仁 <taakaaneo@gmail.com>
2020-04-16 10:31:03 +0900

    feat: update build-config (in environment variable)

--- neo-cycle-client/config/development.env.js
+++ neo-cycle-client/config/development.env.js
@@ -2,5 +2,5 @@ require('dotenv').config();
 
 module.exports = {
   NODE_ENV: '"dev"',
-  VUE_APP_TEST: JSON.stringify(process.env.VUE_APP_TEST)
+  VUE_APP_GOOGLE_API_KEY: JSON.stringify(process.env.VUE_APP_GOOGLE_API_KEY)
 }
