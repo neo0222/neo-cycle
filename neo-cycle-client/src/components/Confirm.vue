1317f27b97db6926f4227cdc5cd072dc46fc343f
長根尾貴仁 <taakaaneo@gmail.com>
2020-04-19 23:22:59 +0900

    feat: revise confiirmation form

--- neo-cycle-client/src/components/Confirm.vue
+++ neo-cycle-client/src/components/Confirm.vue
@@ -4,12 +4,12 @@
     <el-form>
       <div class="input-form-wrapper">
         <el-form-item>
-          <el-input style="width: 440px; max-width: 70% font-size: 16px; transform: scale(0.8);" type="text" placeholder="user ID" v-model="username"/>
+          <el-input style="width: 440px; max-width: 70%; font-size: 16px; transform: scale(0.8);" type="text" placeholder="user ID" v-model="username"/>
         </el-form-item>
       </div>
       <div class="input-form-wrapper">
         <el-form-item>
-          <el-input style="width: 440px; max-width: 70% font-size: 16px; transform: scale(0.8);" type="text" placeholder="confirmation code" v-model="confirmationCode"/>
+          <el-input style="width: 440px; max-width: 70%; font-size: 16px; transform: scale(0.8);" type="text" placeholder="confirmation code" v-model="confirmationCode"/>
         </el-form-item>
       </div>
     </el-form>
