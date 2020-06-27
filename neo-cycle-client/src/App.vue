7c373a1cc51975bc7fbd3f660124c99aa6443ccc
長根尾貴仁 <taakaaneo@gmail.com>
2020-06-27 15:37:28 +0900

    feat: footer display only

--- neo-cycle-client/src/App.vue
+++ neo-cycle-client/src/App.vue
@@ -1,6 +1,21 @@
 <template>
   <div id="app">
-    <router-view/>
+    <el-main>
+      <router-view/>
+    </el-main>
+    <el-footer>
+        <div class="footer-internal">
+          <el-button type="text" style="font-size:30px; width: 29vw;">
+            <i class="el-icon-tickets"></i>
+          </el-button>
+          <el-button type="text" style="font-size:30px; width: 29vw;">
+            <i class="el-icon-map-location"></i>
+          </el-button>
+          <el-button type="text" style="font-size:30px; width: 29vw;">
+            <i class="el-icon-setting"></i>
+          </el-button>
+        </div>
+    </el-footer>
   </div>
 </template>
 
@@ -30,4 +45,22 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+.el-main {
+  padding: 0px;
+  display: flex;
+  min-height: 100vh;
+}
+
+.el-footer {
+  position: fixed;
+  bottom: 0;
+  background-color: #FFFFFF;
+  padding: 0px;
+}
+
+.footer-internal {
+  width: 96vw;
+}
+
 </style>
