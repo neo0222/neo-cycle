f1b528c89d11eadb49e85b10539dfcaaa52b9c6f
長根尾貴仁 <taakaaneo@gmail.com>
2020-09-13 00:15:34 +0900

    feat: add new env(it1~staging5)

--- neo-cycle-client/src/environment/it1.js
+++ neo-cycle-client/src/environment/it1.js
@@ -1,9 +1,9 @@
 const config = {
   Region: 'ap-northeast-1',
-  UserPoolId: 'ap-northeast-1_QQRHYdhFC',
-  ClientId: '1bjjhjcspk531cm0saf723gu6g',
-  IdentityPoolId: 'ap-northeast-1:2d02fd40-8cf3-4418-8e36-607e5a789642',
-  InvokeUrl: 'https://aaetl7k2qb.execute-api.ap-northeast-1.amazonaws.com/dev',
+  UserPoolId: 'ap-northeast-1_pSYoKR3Xm',
+  ClientId: '6ochjtfshe1ghs77ht8o6afcng',
+  IdentityPoolId: 'ap-northeast-1:94ba0be1-3913-4ffc-b3e7-c6fda8ae8767',
+  InvokeUrl: 'https://dc02ktrxad.execute-api.ap-northeast-1.amazonaws.com/it1',
   policyArn: '',
   RetryLimitMs: 30000,
   RetryIntervalMs: 15000
