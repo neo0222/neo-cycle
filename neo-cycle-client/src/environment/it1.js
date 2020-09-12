5fa5b7ba71746b06d4ce40540fde0484f68df1a7
長根尾貴仁 <taakaaneo@gmail.com>
2020-09-12 20:02:33 +0900

    feat: add env & it1

---
+++ neo-cycle-client/src/environment/it1.js
@@ -0,0 +1,12 @@
+const config = {
+  Region: 'ap-northeast-1',
+  UserPoolId: 'ap-northeast-1_QQRHYdhFC',
+  ClientId: '1bjjhjcspk531cm0saf723gu6g',
+  IdentityPoolId: 'ap-northeast-1:2d02fd40-8cf3-4418-8e36-607e5a789642',
+  InvokeUrl: 'https://aaetl7k2qb.execute-api.ap-northeast-1.amazonaws.com/dev',
+  policyArn: '',
+  RetryLimitMs: 30000,
+  RetryIntervalMs: 15000
+}
+
+module.exports = config 
